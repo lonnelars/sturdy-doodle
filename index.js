b08eabc19b9b9f513b17be16b907b9e1ef010381
Lars Lønne <lars.lonne@kantega.no>
2016-05-26 21:17:29 +0200

    feat(axes): adds x and y axes to the chart

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var width = 960;
-var height = 500;
+var margin = {top: 20, right: 30, bottom: 30, left: 40};
+var width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
@@ -7,14 +8,39 @@ var x = d3.scale.ordinal()
 var y = d3.scale.linear()
     .range([height, 0]);
 
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10, "%");
+
 var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g");
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
 d3.tsv("data.tsv", type, (error, data) => {
     x.domain(data.map((d) => d.letter));
     y.domain([0, d3.max(data, (d) => d.frequency)]);
+
+    chart.append("g")
+        .attr("class", "x axis")
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis);
+
+    chart.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Frequency");
 
     var bar = chart.selectAll(".bar")
         .data(data)
